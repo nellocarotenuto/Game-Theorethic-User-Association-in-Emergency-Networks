10ba9c5c103c86ed83f741626762fe8dbf374fb2
Nello Carotenuto <nello.carotenuto.96@live.com>
2020-12-10 00:54:20 +0100

    Placement: Users: accept lat-lon parameters to select the area to consider

--- placement/user_placement.m
+++ placement/user_placement.m
@@ -1,22 +1,28 @@
-function users_table = user_placement(x_range, y_range, number_of_users)
+function users_table = user_placement(vertex_1, vertex_2, altitude, number_of_users)
 %USER_PLACEMENT  Randomly places users on the plane.
-%   This function allows to place a desired number of users in the area of
-%   the plane specified by range intervals.
+%   This function allows to place a desired number of users in the
+%   rectangular area delimited by the two diagonally opposed vertices
+%   parameters.
 % Inputs:
-%   x_range           : a 1x2 vector indicating the range of values for the
-%                       x component (not necessarily ordered)
-%   y_range           : a 1x2 vector indicating the range of values for the
-%                       y component (not necessarily ordered)
-%   number_of_users   : the desired number of users to place on the plane
+%   vertex_1   : a 1-by-2 vector with latitude and longitude components of
+%                the first point that will be used to select the area of
+%                operation
+%   vertex_2   : a 1-by-2 vector with latitude and longitude components of
+%                the second point that will be used to select the area of
+%                operation (diagonally opposed to the first)
+%   altitude   : the average altitude of the area of interest, expressed in
+%                meters
 % Outputs:
 %   users_table   : a table with id, x, y, tower, tower_mean_loss, 
-%                   tower_ratio, uav, uav_mean_loss, uav_ratio, peer,
-%                   peer_mean_loss and peer_ratio properties of the users
-%                   placed on the plane
+%                   tower_ratio, tower_mean_energy, uav, uav_mean_loss,
+%                   uav_ratio, peer, peer_mean_loss, peer_ratio and
+%                   peer_mean_energy properties of the users placed on the
+%                   plane
 
-if ~isequal(size(x_range), [1, 2]) || ~isequal(size(y_range), [1, 2])
-    error("Input parameters should be 1x2 arrays for both x and y ranges.");
-end
+origin = [min(vertex_1(1), vertex_2(1)) + (max(vertex_1(1), vertex_2(1)) - min(vertex_1(1), vertex_2(1))) / 2, ...
+          min(vertex_1(2), vertex_2(2)) + (max(vertex_1(2), vertex_2(2)) - min(vertex_1(2), vertex_2(2))) / 2, altitude];
+
+[x_range, y_range] = latlon2local([vertex_1(1), vertex_2(1)], [vertex_1(2), vertex_2(2)], altitude, origin);
 
 ids = uint16([1 : number_of_users]');
 
@@ -39,6 +45,6 @@ peers_traffic_ratio = zeros(number_of_users, 1);
 peers_mean_energy = zeros(number_of_users, 1);
 
 users_table = table(ids, x, y, towers, towers_mean_loss, towers_traffic_ratio, towers_mean_energy, uavs, uavs_mean_loss, uavs_traffic_ratio, uavs_mean_energy, peers, peers_mean_loss, peers_traffic_ratio, peers_mean_energy, ...
-                    'VariableNames', ["id", "x", "y", "tower", "tower_mean_loss", "tower_ratio", "uav", "uav_mean_loss", "uav_ratio", "peer", "peer_mean_loss", "peer_ratio"]);
+                    'VariableNames', ["id", "x", "y", "tower", "tower_mean_loss", "tower_ratio", "tower_mean_energy", "uav", "uav_mean_loss", "uav_ratio", "uav_mean_energy", "peer", "peer_mean_loss", "peer_ratio", "peer_mean_energy"]);
 
 end
