c250234ac1b6b4c4ae3d018a3ee1cfd1d8dd9adc
Nello Carotenuto <nello.carotenuto.96@live.com>
2020-12-10 00:46:53 +0100

    Placement: Towers: only consider TIM's LTE or UMTS ones

--- placement/tower_placement.m
+++ placement/tower_placement.m
@@ -1,24 +1,25 @@
-function towers_table = tower_placement(vertex_1, vertex_2)
+function towers_table = tower_placement(vertex_1, vertex_2, altitude)
 %TOWER_PLACEMENT  Returns the tower cells a specific area.
 %   This function returns placement and coverage info about tower cells in
 %   the rectangular area delimited by the two diagonally opposed vertices
 %   parameters.
 % Inputs:
 %   vertex_1   : a 1-by-2 vector with latitude and longitude components of
-%                the point that will be used as origin
+%                the first point that will be used to select the area of
+%                operation
 %   vertex_2   : a 1-by-2 vector with latitude and longitude components of
-%                the point diagonally opposed to the origin
+%                the second point that will be used to select the area of
+%                operation (diagonally opposed to the first)
+%   altitude   : the average altitude of the area of interest, expressed in
+%                meters
 % Outputs:
 %   towers_table   : a table with id, x, y, range, max_users and worsening
 %                    properties of the towers in the area of interest,
 %                    where coordinates and range are expressed in meters
 
-
-if ~isequal(size(vertex_1), [1, 2]) || ~isequal(size(vertex_2), [1, 2])
-    error("Input parameters should be [latitude longitude] arrays.");
-end
-
 input_fields = [["radio", "string"];
+                ["mcc", "double"];
+                ["net", "double"];
                 ["lat", "double"];
                 ["lon", "double"];
                 ["range", "double"]];
@@ -31,26 +32,29 @@ csv_files = dir('data/*.csv');
                       
 for i = 1 : length(csv_files)
     file_towers_table = readtable(sprintf("data/%s", csv_files(i).name), ...
-                            'ReadVariableNames', true);
+                                  'ReadVariableNames', true);
                         
     file_towers_table = file_towers_table( ...
                             file_towers_table.lat >= min(vertex_1(1), vertex_2(1)) & ...
                             file_towers_table.lat <= max(vertex_1(1), vertex_2(1)) & ...
                             file_towers_table.lon >= min(vertex_1(2), vertex_2(2)) & ...
-                            file_towers_table.lon <= max(vertex_1(2), vertex_2(2)), ["radio", "lat", "lon", "range"]);
+                            file_towers_table.lon <= max(vertex_1(2), vertex_2(2)), ["radio", "mcc", "net", "lat", "lon", "range"]);
     
     full_towers_table = vertcat(full_towers_table, file_towers_table);
 end
 
-full_towers_table = unique(full_towers_table, 'rows');
+towers_of_interest= (full_towers_table.radio == "LTE" | full_towers_table.radio == "UMTS") & ...
+                     full_towers_table.mcc == 222 & full_towers_table.net == 1;
+                    
+full_towers_table = full_towers_table(towers_of_interest, :);
 
 origin = [min(vertex_1(1), vertex_2(1)) + (max(vertex_1(1), vertex_2(1)) - min(vertex_1(1), vertex_2(1))) / 2, ...
-          min(vertex_1(2), vertex_2(2)) + (max(vertex_1(2), vertex_2(2)) - min(vertex_1(2), vertex_2(2))) / 2, 300];
+          min(vertex_1(2), vertex_2(2)) + (max(vertex_1(2), vertex_2(2)) - min(vertex_1(2), vertex_2(2))) / 2, altitude];
 
 [x, y] = latlon2local(table2array(full_towers_table(: , "lat")), ...
-                      table2array(full_towers_table(: , "lon")), 10, origin);
+                      table2array(full_towers_table(: , "lon")), altitude, origin);
 
-ids = zeros(height(full_towers_table), 1, 'uint16');
+ids = uint16([1 : height(full_towers_table)]');
 range = table2array(full_towers_table(: , "range"));
 worsening = zeros(height(full_towers_table), 1);
 max_users = randi([750 1250], height(full_towers_table), 1);
