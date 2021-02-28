e4d70def4ae04d1df40105563a449beea196f2d7
Nello Carotenuto <nello.carotenuto.96@live.com>
2021-02-28 02:03:54 +0100

    Placement: add battery info for users and remove unnecessary fields

--- placement/user_placement.m
+++ placement/user_placement.m
@@ -13,11 +13,8 @@ function users_table = user_placement(vertex_1, vertex_2, altitude, number_of_us
 %   altitude   : the average altitude of the area of interest, expressed in
 %                meters
 % Outputs:
-%   users_table   : a table with id, x, y, tower, tower_mean_loss, 
-%                   tower_ratio, tower_mean_energy, uav, uav_mean_loss,
-%                   uav_ratio, peer, peer_mean_loss, peer_ratio and
-%                   peer_mean_energy properties of the users placed on the
-%                   plane
+%   users_table   : a table with id, x, y and battery properties of the
+%                   users placed on the plane
 
 origin = [min(vertex_1(1), vertex_2(1)) + (max(vertex_1(1), vertex_2(1)) - min(vertex_1(1), vertex_2(1))) / 2, ...
           min(vertex_1(2), vertex_2(2)) + (max(vertex_1(2), vertex_2(2)) - min(vertex_1(2), vertex_2(2))) / 2, altitude];
@@ -29,22 +26,9 @@ ids = uint16([1 : number_of_users]');
 x = min(x_range) + (max(x_range) - min(x_range)) .* rand(number_of_users, 1);
 y = min(y_range) + (max(y_range) - min(y_range)) .* rand(number_of_users, 1);
 
-towers = zeros(number_of_users, 1, 'uint16');
-towers_mean_loss = zeros(number_of_users, 1);
-towers_traffic_ratio = zeros(number_of_users, 1);
-towers_mean_energy = zeros(number_of_users, 1);
+battery = rand(number_of_users, 1);
 
-uavs = zeros(number_of_users, 1, 'uint16');
-uavs_mean_loss = zeros(number_of_users, 1);
-uavs_traffic_ratio = zeros(number_of_users, 1);
-uavs_mean_energy = zeros(number_of_users, 1);
-
-peers = zeros(number_of_users, 1, 'uint16');
-peers_mean_loss = zeros(number_of_users, 1);
-peers_traffic_ratio = zeros(number_of_users, 1);
-peers_mean_energy = zeros(number_of_users, 1);
-
-users_table = table(ids, x, y, towers, towers_mean_loss, towers_traffic_ratio, towers_mean_energy, uavs, uavs_mean_loss, uavs_traffic_ratio, uavs_mean_energy, peers, peers_mean_loss, peers_traffic_ratio, peers_mean_energy, ...
-                    'VariableNames', ["id", "x", "y", "tower", "tower_mean_loss", "tower_ratio", "tower_mean_energy", "uav", "uav_mean_loss", "uav_ratio", "uav_mean_energy", "peer", "peer_mean_loss", "peer_ratio", "peer_mean_energy"]);
+users_table = table(ids, x, y, battery, ...
+                    'VariableNames', ["id", "x", "y", "battery"]);
 
 end
